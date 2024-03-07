4d3f1c9001e6db1676d710c69bb70f9229682bff
Lucas Quadros <1lucascq@gmail.com.br>
2024-03-07 14:39:27 -0300

    feat: initial commit for login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { API_LINK } from "../helpers";
 
 function LoginForm() {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	console.log(username, password)
+
+	async function test() {
+		const response = await fetch(`${API_LINK}smartphones`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		console.log('oi')
+		console.log(response)
+	}
+	test()
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await fetch(`${API_LINK}login`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ username, password }),
+			});
+			console.log(response)
+			if (response.ok) {
+				console.log("Login successful");
+			} else {
+				console.error("Login failed");
+			}
+		} catch (error) {
+			console.error("Error:", error);
+		}
+	};
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<div className='form-group'>
 				<label htmlFor='username'>Username</label>
-				<input type='text' id='username' name='username' />
+				<input type='text' id='username' name='username' onChange={(e) => setUsername(e.target.value)} />
 			</div>
 			<div className='form-group'>
 				<label htmlFor='password'>Password</label>
-				<input type='password' id='password' name='password' />
+				<input type='password' id='password' name='password' onChange={(e) => setPassword(e.target.value)} />
 			</div>
 			<button type='submit'>Login</button>
 		</form>
---
+++ src/helpers/index.js
@@ -0,0 +1,1 @@
+export const API_LINK = 'https://lexart-labs-backend.vercel.app/api/'
